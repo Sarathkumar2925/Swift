40b224750fc6fc598685a0a1b0fd3fb547dd572d
Sarathkumar2925 <51906735+Sarathkumar2925@users.noreply.github.com>
2019-09-18 12:35:31 +0530

    Create positiveNegativeZero.swift

---
+++ positiveNegativeZero.swift
@@ -0,0 +1,14 @@
+var scan = readLine()!
+var number = Int(scan)!
+if(number > 0)
+{
+    print("The number is positive")
+}
+else if (number < 0)
+{
+    print("The number is negative")
+}
+else if(number == 0)
+{
+    print("The number is equal to zero")
+}
