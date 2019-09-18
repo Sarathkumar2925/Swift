a2f2bcf5bf7bf3fae407da2795cf54f3553d4ebe
Sarathkumar2925 <51906735+Sarathkumar2925@users.noreply.github.com>
2019-09-18 12:53:11 +0530

    Add files via upload

---
+++ oddEven.swift
@@ -0,0 +1,12 @@
+print("Enter the number ")
+var scan = readLine()!
+var number = Int(scan)!
+if number % 2 == 0
+{
+    print("\(number) is even")
+}
+
+else 
+{    
+    print("\(number) is odd")
+}
