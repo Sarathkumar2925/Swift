a26dc5445382af588cab1fd2cab47b854a2d328d
Sarathkumar2925 <51906735+Sarathkumar2925@users.noreply.github.com>
2019-09-18 12:42:04 +0530

    Add files via upload

---
+++ greaterNumber.swift
@@ -0,0 +1,15 @@
+print("Enter the number :")
+var scan = readLine()!
+var num1 = Int(scan)!
+print("Enter the number :")
+scan = readLine()!
+var num2 = Int(scan)!
+if num1 > num2
+{
+    print("\(num1) is greater than \(num2)")    
+}
+else 
+{
+    print("\(num2) is greater than \(num1)")  
+}
+
